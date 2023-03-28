391f07b7360a6e3c7b446a8521f9e50ddfa358d0
lxer19 <lxerleap@gmail.com>
2023-03-28 00:21:50 -0700

    simplify tran functions

--- train_and_eval.py
+++ train_and_eval.py
@@ -140,6 +140,248 @@ def evaluate_mini_batch(
 2. Run teacher
 """
 
+def get_train_eval_datasets_sage(g,idx_train,batch_size,fan_out,num_workers):
+    # Create dataloader for SAGE
+
+    # Create csr/coo/csc formats before launching sampling processes
+    # This avoids creating certain formats in each data loader process, which saves momory and CPU.
+    g.create_formats_()
+    sampler = dgl.dataloading.MultiLayerNeighborSampler(
+        [eval(fanout) for fanout in fan_out.split(",")]
+    )
+    dataloader = dgl.dataloading.NodeDataLoader(
+        g,
+        idx_train,
+        sampler,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=False,
+        num_workers=num_workers,
+    )
+
+    # SAGE inference is implemented as layer by layer, so the full-neighbor sampler only collects one-hop neighors
+    sampler_eval = dgl.dataloading.MultiLayerFullNeighborSampler(1)
+    dataloader_eval = dgl.dataloading.NodeDataLoader(
+        g,
+        torch.arange(g.num_nodes()),
+        sampler_eval,
+        batch_size=batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers,
+    )
+
+    data = dataloader
+    data_eval = dataloader_eval
+    return data,data_eval
+
+def print_debug_info(epoch,loss, loss_train,loss_val,loss_test,score_train,score_val,score_test,logger, loss_and_score):
+    logger.debug(
+        f"Ep {epoch:3d} | loss: {loss:.4f} | s_train: {score_train:.4f} | s_val: {score_val:.4f} | s_test: {score_test:.4f}"
+    )
+    loss_and_score += [
+        [
+            epoch,
+            loss_train,
+            loss_val,
+            loss_test,
+            score_train,
+            score_val,
+            score_test,
+        ]
+    ]
+
+def early_stop_counter(count,score_val,best_score_val,epoch,model):
+    if score_val >= best_score_val:
+        best_epoch = epoch
+        best_score_val = score_val
+        state = copy.deepcopy(model.state_dict())
+        count = 0
+    else:
+        count += 1
+    return count,state
+
+def eval_on_train_val_test_data_gnn(model, data_eval, feats, labels, criterion, evaluator, idx_train,idx_val,idx_test):
+    out, loss_train, score_train = evaluate(
+        model, data_eval, feats, labels, criterion, evaluator, idx_train
+    )
+    # Use criterion & evaluator instead of evaluate to avoid redundant forward pass
+    loss_val = criterion(out[idx_val], labels[idx_val]).item()
+    score_val = evaluator(out[idx_val], labels[idx_val])
+    loss_test = criterion(out[idx_test], labels[idx_test]).item()
+    score_test = evaluator(out[idx_test], labels[idx_test])
+    return loss_train, score_train,loss_val,score_val,loss_test,score_test
+
+def eval_on_train_val_test_data_mlp(model, feats_train,labels_train,feats_val, labels_val,feats_test, labels_test,criterion,batch_size,evaluator):
+    _, loss_train, score_train = evaluate_mini_batch(
+        model, feats_train, labels_train, criterion, batch_size, evaluator
+    )
+    _, loss_val, score_val = evaluate_mini_batch(
+        model, feats_val, labels_val, criterion, batch_size, evaluator
+    )
+    _, loss_test, score_test = evaluate_mini_batch(
+        model, feats_test, labels_test, criterion, batch_size, evaluator
+    )
+    return loss_train, score_train,loss_val,score_val,loss_test,score_test
+
+def run_transductive_sage(
+    conf,
+    model,
+    g,
+    feats,
+    labels,
+    indices,
+    criterion,
+    evaluator,
+    optimizer,
+    logger,
+    loss_and_score,
+):
+    idx_train, idx_val, idx_test = indices
+    batch_size = conf["batch_size"]
+    data,data_eval= get_train_eval_datasets_sage(g,idx_train,batch_size,conf["fan_out"],conf["num_workers"])
+    best_epoch, best_score_val, count = 0, 0, 0
+    for epoch in range(1, conf["max_epoch"] + 1):
+        loss = train_sage(model, data, feats, labels, criterion, optimizer)
+        if epoch % conf["eval_interval"] == 0:
+            (
+                loss_train, 
+                score_train,
+                loss_val,
+                score_val,
+                loss_test,
+                score_test
+            )=eval_on_train_val_test_data_gnn(model, data_eval, feats, labels, criterion, evaluator, idx_train,idx_val,idx_test)
+            print_debug_info(epoch,loss, loss_train,loss_val,loss_test,score_train,score_val,score_test,logger, loss_and_score)
+            count,state=early_stop_counter(count,score_val,best_score_val,epoch,model)
+
+        if count == conf["patience"] or epoch == conf["max_epoch"]:
+            break
+    model.load_state_dict(state)
+    out, _, score_val = evaluate(
+        model, data_eval, feats, labels, criterion, evaluator, idx_val
+    )
+    score_test = evaluator(out[idx_test], labels[idx_test])
+    logger.info(
+        f"Best valid model at epoch: {best_epoch: 3d}, score_val: {score_val :.4f}, score_test: {score_test :.4f}"
+    )
+    return out, score_val, score_test
+
+def run_transductive_mlp(
+    conf,
+    model,
+    feats,
+    labels,
+    indices,
+    criterion,
+    evaluator,
+    optimizer,
+    logger,
+    loss_and_score,
+):
+    batch_size = conf["batch_size"]
+    idx_train, idx_val, idx_test = indices
+    feats_train, labels_train = feats[idx_train], labels[idx_train]
+    feats_val, labels_val = feats[idx_val], labels[idx_val]
+    feats_test, labels_test = feats[idx_test], labels[idx_test]
+    best_epoch, best_score_val, count = 0, 0, 0
+    for epoch in range(1, conf["max_epoch"] + 1):
+        loss = train_mini_batch(
+            model, feats_train, labels_train, batch_size, criterion, optimizer
+        )
+        if epoch % conf["eval_interval"] == 0:
+            (
+                loss_train, 
+                score_train,
+                loss_val,
+                score_val,
+                loss_test,
+                score_test
+            )=eval_on_train_val_test_data_mlp(model, feats_train,labels_train,feats_val, labels_val,feats_test, labels_test,criterion,batch_size,evaluator)
+            print_debug_info(epoch,loss, loss_train,loss_val,loss_test,score_train,score_val,score_test,logger, loss_and_score)
+            count,state=early_stop_counter(count,score_val,best_score_val,epoch,model)
+
+        if count == conf["patience"] or epoch == conf["max_epoch"]:
+            break
+    model.load_state_dict(state)
+    out, _, score_val = evaluate_mini_batch(
+        model, feats, labels, criterion, batch_size, evaluator, idx_val
+    )
+
+    score_test = evaluator(out[idx_test], labels[idx_test])
+    logger.info(
+        f"Best valid model at epoch: {best_epoch: 3d}, score_val: {score_val :.4f}, score_test: {score_test :.4f}"
+    )
+    return out, score_val, score_test
+
+def run_transductive_gnn(
+    conf,
+    model,
+    g,
+    feats,
+    labels,
+    indices,
+    criterion,
+    evaluator,
+    optimizer,
+    logger,
+    loss_and_score,
+):
+    device = conf["device"]
+    idx_train, idx_val, idx_test = indices
+    batch_size = conf["batch_size"]
+    g = g.to(device)
+    data = g
+    data_eval = g
+    best_epoch, best_score_val, count = 0, 0, 0
+    for epoch in range(1, conf["max_epoch"] + 1):
+        loss = train(model, data, feats, labels, criterion, optimizer, idx_train)
+        if epoch % conf["eval_interval"] == 0:
+            out, loss_train, score_train = evaluate(
+                model, data_eval, feats, labels, criterion, evaluator, idx_train
+            )
+            # Use criterion & evaluator instead of evaluate to avoid redundant forward pass
+            loss_val = criterion(out[idx_val], labels[idx_val]).item()
+            score_val = evaluator(out[idx_val], labels[idx_val])
+            loss_test = criterion(out[idx_test], labels[idx_test]).item()
+            score_test = evaluator(out[idx_test], labels[idx_test])
+            logger.debug(
+                f"Ep {epoch:3d} | loss: {loss:.4f} | s_train: {score_train:.4f} | s_val: {score_val:.4f} | s_test: {score_test:.4f}"
+            )
+            loss_and_score += [
+                [
+                    epoch,
+                    loss_train,
+                    loss_val,
+                    loss_test,
+                    score_train,
+                    score_val,
+                    score_test,
+                ]
+            ]
+
+            if score_val >= best_score_val:
+                best_epoch = epoch
+                best_score_val = score_val
+                state = copy.deepcopy(model.state_dict())
+                count = 0
+            else:
+                count += 1
+
+        if count == conf["patience"] or epoch == conf["max_epoch"]:
+            break
+    model.load_state_dict(state)
+    out, _, score_val = evaluate(
+        model, data_eval, feats, labels, criterion, evaluator, idx_val
+    )
+
+    score_test = evaluator(out[idx_test], labels[idx_test])
+    logger.info(
+        f"Best valid model at epoch: {best_epoch: 3d}, score_val: {score_val :.4f}, score_test: {score_test :.4f}"
+    )
+    return out, score_val, score_test
+
+
 
 def run_transductive(
     conf,
@@ -171,120 +413,46 @@ def run_transductive(
     labels = labels.to(device)
 
     if "SAGE" in model.model_name:
-        # Create dataloader for SAGE
-
-        # Create csr/coo/csc formats before launching sampling processes
-        # This avoids creating certain formats in each data loader process, which saves momory and CPU.
-        g.create_formats_()
-        sampler = dgl.dataloading.MultiLayerNeighborSampler(
-            [eval(fanout) for fanout in conf["fan_out"].split(",")]
-        )
-        dataloader = dgl.dataloading.NodeDataLoader(
+        return run_transductive_sage(
+            conf,
+            model,
             g,
-            idx_train,
-            sampler,
-            batch_size=batch_size,
-            shuffle=True,
-            drop_last=False,
-            num_workers=conf["num_workers"],
+            feats,
+            labels,
+            indices,
+            criterion,
+            evaluator,
+            optimizer,
+            logger,
+            loss_and_score,
         )
-
-        # SAGE inference is implemented as layer by layer, so the full-neighbor sampler only collects one-hop neighors
-        sampler_eval = dgl.dataloading.MultiLayerFullNeighborSampler(1)
-        dataloader_eval = dgl.dataloading.NodeDataLoader(
-            g,
-            torch.arange(g.num_nodes()),
-            sampler_eval,
-            batch_size=batch_size,
-            shuffle=False,
-            drop_last=False,
-            num_workers=conf["num_workers"],
-        )
-
-        data = dataloader
-        data_eval = dataloader_eval
     elif "MLP" in model.model_name:
-        feats_train, labels_train = feats[idx_train], labels[idx_train]
-        feats_val, labels_val = feats[idx_val], labels[idx_val]
-        feats_test, labels_test = feats[idx_test], labels[idx_test]
-    else:
-        g = g.to(device)
-        data = g
-        data_eval = g
-
-    best_epoch, best_score_val, count = 0, 0, 0
-    for epoch in range(1, conf["max_epoch"] + 1):
-        if "SAGE" in model.model_name:
-            loss = train_sage(model, data, feats, labels, criterion, optimizer)
-        elif "MLP" in model.model_name:
-            loss = train_mini_batch(
-                model, feats_train, labels_train, batch_size, criterion, optimizer
-            )
-        else:
-            loss = train(model, data, feats, labels, criterion, optimizer, idx_train)
-
-        if epoch % conf["eval_interval"] == 0:
-            if "MLP" in model.model_name:
-                _, loss_train, score_train = evaluate_mini_batch(
-                    model, feats_train, labels_train, criterion, batch_size, evaluator
-                )
-                _, loss_val, score_val = evaluate_mini_batch(
-                    model, feats_val, labels_val, criterion, batch_size, evaluator
-                )
-                _, loss_test, score_test = evaluate_mini_batch(
-                    model, feats_test, labels_test, criterion, batch_size, evaluator
-                )
-            else:
-                out, loss_train, score_train = evaluate(
-                    model, data_eval, feats, labels, criterion, evaluator, idx_train
-                )
-                # Use criterion & evaluator instead of evaluate to avoid redundant forward pass
-                loss_val = criterion(out[idx_val], labels[idx_val]).item()
-                score_val = evaluator(out[idx_val], labels[idx_val])
-                loss_test = criterion(out[idx_test], labels[idx_test]).item()
-                score_test = evaluator(out[idx_test], labels[idx_test])
-
-            logger.debug(
-                f"Ep {epoch:3d} | loss: {loss:.4f} | s_train: {score_train:.4f} | s_val: {score_val:.4f} | s_test: {score_test:.4f}"
-            )
-            loss_and_score += [
-                [
-                    epoch,
-                    loss_train,
-                    loss_val,
-                    loss_test,
-                    score_train,
-                    score_val,
-                    score_test,
-                ]
-            ]
-
-            if score_val >= best_score_val:
-                best_epoch = epoch
-                best_score_val = score_val
-                state = copy.deepcopy(model.state_dict())
-                count = 0
-            else:
-                count += 1
-
-        if count == conf["patience"] or epoch == conf["max_epoch"]:
-            break
-
-    model.load_state_dict(state)
-    if "MLP" in model.model_name:
-        out, _, score_val = evaluate_mini_batch(
-            model, feats, labels, criterion, batch_size, evaluator, idx_val
+        return run_transductive_mlp(
+            conf,
+            model,
+            feats,
+            labels,
+            indices,
+            criterion,
+            evaluator,
+            optimizer,
+            logger,
+            loss_and_score,
         )
     else:
-        out, _, score_val = evaluate(
-            model, data_eval, feats, labels, criterion, evaluator, idx_val
+        return run_transductive_gnn(
+            conf,
+            model,
+            g,
+            feats,
+            labels,
+            indices,
+            criterion,
+            evaluator,
+            optimizer,
+            logger,
+            loss_and_score,
         )
-
-    score_test = evaluator(out[idx_test], labels[idx_test])
-    logger.info(
-        f"Best valid model at epoch: {best_epoch: 3d}, score_val: {score_val :.4f}, score_test: {score_test :.4f}"
-    )
-    return out, score_val, score_test
 
 
 def run_inductive(
@@ -602,7 +770,129 @@ def distill_run_transductive(
     return out, score_val, score_test
 
 
-def distill_run_inductive(
+def distill_run_inductive_gnn(
+    conf,
+    model,
+    feats,
+    labels,
+    out_t_all,
+    distill_indices,
+    criterion_l,
+    criterion_t,
+    evaluator,
+    optimizer,
+    logger,
+    loss_and_score,
+):
+    set_seed(conf["seed"])
+    device = conf["device"]
+    batch_size = conf["batch_size"]
+    lamb = conf["lamb"]
+    (
+        obs_idx_l,
+        obs_idx_t,
+        obs_idx_val,
+        obs_idx_test,
+        idx_obs,
+        idx_test_ind,
+    ) = distill_indices
+
+    feats = feats.to(device)
+    labels = labels.to(device)
+    out_t_all = out_t_all.to(device)
+    obs_feats = feats[idx_obs]
+    obs_labels = labels[idx_obs]
+    obs_out_t = out_t_all[idx_obs]
+
+    feats_l, labels_l = obs_feats[obs_idx_l], obs_labels[obs_idx_l]
+    feats_t, out_t = obs_feats[obs_idx_t], obs_out_t[obs_idx_t]
+    feats_val, labels_val = obs_feats[obs_idx_val], obs_labels[obs_idx_val]
+    feats_test_tran, labels_test_tran = (
+        obs_feats[obs_idx_test],
+        obs_labels[obs_idx_test],
+    )
+    feats_test_ind, labels_test_ind = feats[idx_test_ind], labels[idx_test_ind]
+
+    best_epoch, best_score_val, count = 0, 0, 0
+    for epoch in range(1, conf["max_epoch"] + 1):
+        loss_l = train_mini_batch(
+            model, feats_l, labels_l, batch_size, criterion_l, optimizer, lamb
+        )
+        loss_t = train_mini_batch(
+            model, feats_t, out_t, batch_size, criterion_t, optimizer, 1 - lamb
+        )
+        loss = loss_l + loss_t
+        if epoch % conf["eval_interval"] == 0:
+            _, loss_l, score_l = evaluate_mini_batch(
+                model, feats_l, labels_l, criterion_l, batch_size, evaluator
+            )
+            _, loss_val, score_val = evaluate_mini_batch(
+                model, feats_val, labels_val, criterion_l, batch_size, evaluator
+            )
+            _, loss_test_tran, score_test_tran = evaluate_mini_batch(
+                model,
+                feats_test_tran,
+                labels_test_tran,
+                criterion_l,
+                batch_size,
+                evaluator,
+            )
+            _, loss_test_ind, score_test_ind = evaluate_mini_batch(
+                model,
+                feats_test_ind,
+                labels_test_ind,
+                criterion_l,
+                batch_size,
+                evaluator,
+            )
+
+            logger.debug(
+                f"Ep {epoch:3d} | l: {loss:.4f} | s_l: {score_l:.4f} | s_val: {score_val:.4f} | s_tt: {score_test_tran:.4f} | s_ti: {score_test_ind:.4f}"
+            )
+            loss_and_score += [
+                [
+                    epoch,
+                    loss_l,
+                    loss_val,
+                    loss_test_tran,
+                    loss_test_ind,
+                    score_l,
+                    score_val,
+                    score_test_tran,
+                    score_test_ind,
+                ]
+            ]
+
+            if score_val >= best_score_val:
+                best_epoch = epoch
+                best_score_val = score_val
+                state = copy.deepcopy(model.state_dict())
+                count = 0
+            else:
+                count += 1
+
+        if count == conf["patience"] or epoch == conf["max_epoch"]:
+            break
+
+    model.load_state_dict(state)
+    obs_out, _, score_val = evaluate_mini_batch(
+        model, obs_feats, obs_labels, criterion_l, batch_size, evaluator, obs_idx_val
+    )
+    out, _, score_test_ind = evaluate_mini_batch(
+        model, feats, labels, criterion_l, batch_size, evaluator, idx_test_ind
+    )
+
+    # Use evaluator instead of evaluate to avoid redundant forward pass
+    score_test_tran = evaluator(obs_out[obs_idx_test], labels_test_tran)
+    out[idx_obs] = obs_out
+
+    logger.info(
+        f"Best valid model at epoch: {best_epoch: 3d} score_val: {score_val :.4f}, score_test_tran: {score_test_tran :.4f}, score_test_ind: {score_test_ind :.4f}"
+    )
+    return out, score_val, score_test_tran, score_test_ind
+    
+
+def distill_run_inductive_mle(
     conf,
     model,
     feats,
@@ -736,3 +1026,33 @@ def distill_run_inductive(
         f"Best valid model at epoch: {best_epoch: 3d} score_val: {score_val :.4f}, score_test_tran: {score_test_tran :.4f}, score_test_ind: {score_test_ind :.4f}"
     )
     return out, score_val, score_test_tran, score_test_ind
+
+def distill_run_inductive(
+    conf,
+    model,
+    feats,
+    labels,
+    out_t_all,
+    distill_indices,
+    criterion_l,
+    criterion_t,
+    evaluator,
+    optimizer,
+    logger,
+    loss_and_score,
+):
+    if model.model_name=="MLE":
+        return distill_run_inductive_mle(
+            conf,
+            model,
+            feats,
+            labels,
+            out_t_all,
+            distill_indices,
+            criterion_l,
+            criterion_t,
+            evaluator,
+            optimizer,
+            logger,
+            loss_and_score,
+        )
